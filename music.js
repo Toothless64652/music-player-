b3615706ca28c5ed6785951018e48c9b9eccd62e
Toothlessgrin <79648456+Toothless64652@users.noreply.github.com>
2023-04-06 10:55:27 +0100

    Add files via upload

---
+++ music.js
@@ -0,0 +1,62 @@
+let allMusic = [    
+    {
+        name: "Here",
+        artist: "Alessia cara",
+        img: "img-0",
+        src: "music-0"
+    },
+    {
+        name: "Seventeen",
+        artist: "Alessia cara",
+        img: "img-1",
+        src: "music-1"
+    },
+    {
+        name: "How Far I'll Go",
+        artist: "Alessia cara",
+        img: "img-2",
+        src: "music-2"
+    },
+    {
+        name: "My Songs",
+        artist: "Alessia cara",
+        img: "img-3",
+        src: "music-3"
+    },
+    {
+        name: "River Of Tears",
+        artist: "Alessia cara",
+        img: "img-4",
+        src: "music-4"
+    },
+    {
+        name: "Scar To Your Beautiful",
+        artist: "Alessia cara",
+        img: "img-5",
+        src: "music-5"
+    },
+    {
+        name: "Stars",
+        artist: "Alessia cara",
+        img: "img-6",
+        src: "music-6"
+    },
+    {
+        name: "Overdose",
+        artist: "Alessia cara",
+        img: "img-7",
+        src: "music-7"
+    },
+    {
+        name: "Stones",
+        artist: "Alessia cara",
+        img: "img-8",
+        src: "music-8"
+    },
+    {
+        name: "Outlaw",
+        artist: "Alessia cara",
+        img: "img-9",
+        src: "music-9"
+    },
+];
